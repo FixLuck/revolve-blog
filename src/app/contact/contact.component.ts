ada049085d1900c14b078f26853ba62a4e5db273
FixLuck <caube.vodoi5@gmail.com>
2024-01-29 15:18:03 +0700

    add validator

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  constructor(private fb: FormBuilder){}
 
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.email]]
+  })
 }
